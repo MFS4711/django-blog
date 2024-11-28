9315a79a8b4f3296be57fd72635a941f472ab45f
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-28 17:04:04 +0000

    Create view.py for about app with a function-based apporach

--- about/views.py
+++ about/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import About
 
 # Create your views here.
+def about(request):
+    queryset = About.objects.all().order_by('-updated_on').first()
+    about = get_object_or_404(queryset)
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
