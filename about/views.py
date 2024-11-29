89d8181998f9121ca0a448d19aa85e492869efd2
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-29 12:28:25 +0000

    Create form in about app - add forms.py and add model for form - initialise form class in views.py and write relevant HTML and DTL to make form functional and visible

--- about/views.py
+++ about/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import About
+from .forms import CollaborateRequestForm
 
 # Create your views here.
 def about_me(request):
 
     about = About.objects.all().order_by('-updated_on').first()
 
+    collaborate_request_form = CollaborateRequestForm()
+
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_request_form": collaborate_request_form,
+        },
     )
