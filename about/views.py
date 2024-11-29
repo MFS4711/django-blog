77f3cf86cef59bf255e8617f031f6540961ebadd
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-29 12:45:04 +0000

    Wire up form to give fnctionality with if statement in views.py and import messages at top of views.py to display message

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateRequestForm
 
@@ -6,6 +7,15 @@ from .forms import CollaborateRequestForm
 def about_me(request):
 
     about = About.objects.all().order_by('-updated_on').first()
+
+    if request.method == "POST":
+        collaborate_request_form = CollaborateRequestForm(data=request.POST)
+        if collaborate_request_form.is_valid():
+            collaborate_request.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
 
     collaborate_request_form = CollaborateRequestForm()
 
